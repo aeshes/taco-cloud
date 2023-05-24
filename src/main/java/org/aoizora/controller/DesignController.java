588a4007fbb1e1c3c8d2b6a5b3989e531ee593bb
Aoizora <mathesis@inbox.ru>
2023-05-24 16:47:39 +0300

    Добавил страницу заказа

--- src/main/java/org/aoizora/controller/DesignController.java
+++ src/main/java/org/aoizora/controller/DesignController.java
@@ -7,10 +7,7 @@ import org.aoizora.model.tacos.Taco;
 import org.aoizora.model.tacos.TacoOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -70,5 +67,14 @@ public class DesignController
         return ingredients.stream()
                 .filter(it -> it.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder)
+    {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/order/current";
     }
 }
