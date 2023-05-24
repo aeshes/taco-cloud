b9aab2f03c42859a42ada2c34d76049630bf4cc6
Aoizora <mathesis@inbox.ru>
2023-05-24 14:56:59 +0300

    Первый коммит

---
+++ src/main/java/org/aoizora/controller/DesignController.java
@@ -0,0 +1,74 @@
+package org.aoizora.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aoizora.model.tacos.Ingredient;
+import org.aoizora.model.tacos.Ingredient.Type;
+import org.aoizora.model.tacos.Taco;
+import org.aoizora.model.tacos.TacoOrder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.aoizora.model.tacos.Ingredient.Type.*;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignController
+{
+    @ModelAttribute
+    public void addIngredientsToModel(Model model)
+    {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", WRAP),
+                new Ingredient("COTO", "Corn Tortilla", WRAP),
+                new Ingredient("GRBF", "Ground Beef", PROTEIN),
+                new Ingredient("CARN", "Carnitas", PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", VEGGIES),
+                new Ingredient("LETC", "Lettuce", VEGGIES),
+                new Ingredient("CHED", "Cheddar", CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", CHEESE),
+                new Ingredient("SLSA", "Salsa", SAUCE),
+                new Ingredient("SRCR", "Sour Cream", SAUCE)
+        );
+
+        Type[] types = Type.values();
+        for (Type type : types)
+        {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+    }
+
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order()
+    {
+        return new TacoOrder();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco()
+    {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm()
+    {
+        return "design";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type)
+    {
+        return ingredients.stream()
+                .filter(it -> it.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
